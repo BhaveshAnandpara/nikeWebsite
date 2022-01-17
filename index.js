917eef98df7068b01f9841ad26df394bf7a4b2ad
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-01-17 16:32:13 +0530

    Mobile Responsive

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const element = document.querySelector(".hor-scroll");
 const slider = document.querySelector(".slider");
 
+const section = document.getElementsByTagName("section")
+const shoe = document.querySelector("#hero-shoe")
+
 let height = window.innerHeight;
+
+if(window.innerWidth < 480){
+    
+    shoeSize()
+    
+}
+
+else{
+
+    element.addEventListener('wheel', (event) => {
+        event.preventDefault();
+        
+        element.scrollBy({
+            left: event.deltaY < 0 ? -100 : 100,
+        });
+        if( element.scrollLeft === 0 ){
+            
+            window.scrollTo(0, 1100);
+            
+        }
+        
+        
+    });
+
+}
 
 function scrollToCTA(){
 
@@ -9,18 +37,8 @@ function scrollToCTA(){
 
 }
 
-
-element.addEventListener('wheel', (event) => {
-  event.preventDefault();
-  
-  element.scrollBy({
-    left: event.deltaY < 0 ? -100 : 100,
-});
-    if( element.scrollLeft === 0 ){
-
-        window.scrollTo(0, 1100);
-
-    }
-
-
-});
+function shoeSize(){
+    
+    shoe.style.transform = "scale(0.3) rotate(-22.93deg)"
+    
+}
